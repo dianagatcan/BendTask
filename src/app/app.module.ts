0931ef4b92c4869e2a5cccf00ff4cb928d6d720d
dianagatcan <gatcan.diana@gmail.com>
2022-03-30 15:52:58 +0300

    delete zona1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Zona1Component } from './zona1/zona1.component';
 import { ZoneComponent } from './zone/zone.component';
 import { RectangleComponent } from './rectangle/rectangle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Zona1Component,
     ZoneComponent,
     RectangleComponent
   ],
